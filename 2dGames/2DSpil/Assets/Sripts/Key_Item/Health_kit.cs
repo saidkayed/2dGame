9298156a1b71a13cfbd49d5135ad225577a7d326
saidkayed <36620432+saidkayed@users.noreply.github.com>
2019-03-20 09:54:31 +0100

    d

---
+++ 2dGames/2DSpil/Assets/Sripts/Key_Item/Health_kit.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health_kit : MonoBehaviour
+{
+    public float health_kit = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            float hp = Stats.HP + 10;
+
+            Stats.HP = hp;
+
+            if (Stats.HP > 100)
+            {
+                Stats.HP = 100;
+            }
+        }
+        StartCoroutine("destroy");
+    }
+
+    IEnumerator destroy()
+    {
+        yield return new WaitForSeconds(0);
+        Destroy(gameObject);
+    }
+}
